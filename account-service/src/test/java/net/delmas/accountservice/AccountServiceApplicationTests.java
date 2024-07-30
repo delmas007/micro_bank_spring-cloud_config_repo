16818c0e31941376c4840a5d862976ab875cd49c
delmas007 <azertyazerty1ze@gmail.com>
2024-07-30 12:55:19 +0000

    init

---
+++ account-service/src/test/java/net/delmas/accountservice/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.delmas.accountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
